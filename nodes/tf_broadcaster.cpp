77611053b951a51a908ac43f27d697b863c4b2b3
Ottavio Hartman <hartmano@sas.upenn.edu>
2016-06-08 09:28:41 -0400

    Added quaternion transform in order to fix hokuyo space -> world space

--- nodes/tf_broadcaster.cpp
+++ nodes/tf_broadcaster.cpp
@@ -6,13 +6,21 @@
 #include "sensor_msgs/Imu.h"
 
 void handle_imu_rotation(const sensor_msgs::Imu &msg) {
+	// Creat tf broadcaster
 	static tf::TransformBroadcaster br;
 	tf::Transform transform;
 	transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
 	tf::Quaternion q;
 	geometry_msgs::Quaternion quat = msg.orientation;
+	// Conversion function for quaternions
 	quaternionMsgToTF(quat, q);
-	transform.setRotation(q);
+	// Below is crucial transformation -- I don't know why though
+	tf::Vector3 axis = q.getAxis();
+	axis.setY(-axis.y());
+	tf::Quaternion q2(axis, q.getAngle());
+	// Set the transform
+	transform.setRotation(q2);
+	// Publish to tf
 	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "laser", "world"));
 }
 
@@ -20,7 +28,8 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "tf_broadcaster");
 	ros::NodeHandle n;
 	
-	ros::Subscriber sub = n.subscribe("/imu/data", 3, &handle_imu_rotation);
+	// Subscribe to Imu and set callback to publish tf data
+	ros::Subscriber sub = n.subscribe("/imu/data", 4, &handle_imu_rotation);
 
 	ros::spin();
 	return 0;
