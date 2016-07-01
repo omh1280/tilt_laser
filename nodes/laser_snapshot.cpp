5f495d683478fe88be4800f36748d37f50ec1193
Ottavio Hartman <hartmano@sas.upenn.edu>
2016-07-01 14:23:47 -0400

    Tried and failed to make ICP work.

--- nodes/laser_snapshot.cpp
+++ nodes/laser_snapshot.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
 	ros::service::waitForService("assemble_scans2");
 	
 	// Set update frequency (in HZ) to publish point cloud data
-	ros::Rate r(1.5);
+	ros::Rate r(3.);
 
 	// Create publisher
 	ros::Publisher pub = n.advertise<sensor_msgs::PointCloud2>("cloud", 2);
