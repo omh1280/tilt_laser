5c173abf6801874b16274bcab17c6822cd5f6c66
Ottavio Hartman <hartmano@sas.upenn.edu>
2016-07-12 14:42:30 -0400

    Created method to test region_growing.cpp algorithm in a directory.

--- nodes/laser_snapshot.cpp
+++ nodes/laser_snapshot.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
 	ros::service::waitForService("assemble_scans2");
 	
 	// Set update frequency (in HZ) to publish point cloud data
-	ros::Rate r(4.);
+	ros::Rate r(1.);
 
 	// Create publisher
 	ros::Publisher pub = n.advertise<sensor_msgs::PointCloud2>("cloud", 2);
